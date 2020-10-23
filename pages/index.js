331f8dad3bad5cd287f0cdaba79464aaa651b3ca
Koby Conrad <52094714+kobyconrad@users.noreply.github.com>
2020-10-23 11:16:35 -0700

    added presence and mouse listener to proj

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,25 @@
-import { useMap } from "@roomservice/react";
+import { useMap, usePresence } from "@roomservice/react";
+import { useEffect } from "react";
 
 export default function Home() {
   const [map, setMap] = useMap("roomName", "mapName");
+  const [positions, setMyPosition] = usePresence(
+    "presenceRoomName",
+    "positions"
+  );
+
+  useEffect(() => {
+    const listener = (e) => {
+      console.log(e);
+      setMyPosition({
+        x: e.clientX,
+        y: e.clientY,
+      });
+    };
+
+    document.addEventListener("mousemove", listener);
+    return () => document.removeEventListener("mousemove", listener);
+  });
 
   if (!map) return <div>loading...</div>;
 
